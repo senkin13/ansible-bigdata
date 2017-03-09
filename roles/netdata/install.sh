b5cdf71a998d3d80eae5be89bbd526446c76aeba
senkin13 <senkin13@gmail.com>
2017-03-09 18:16:35 +0900

    Update install.sh

--- roles/netdata/install.sh
+++ roles/netdata/install.sh
@@ -1,4 +1,4 @@
-yum install autoconf automake curl gcc git libmnl-devel libuuid-devel lm_sensors make MySQL-python nc pkgconfig python python-psycopg2 PyYAML zlib-devel
+yum -y install autoconf automake curl gcc git libmnl-devel libuuid-devel lm_sensors make MySQL-python nc pkgconfig python python-psycopg2 PyYAML zlib-devel
 cd /opt
 git clone https://github.com/firehol/netdata.git --depth=1
 cd netdata
